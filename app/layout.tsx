ceddfb969a118f3f0efe18ea58fad7bddce47242
senghak <zenghack1@gmail.com>
2026-02-25 03:23:33 +0700

    change logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'クメール関西株式会社 | ちょっと待って！捨てるよりも活かす選択を',
+  title: 'Khmer Kansai Co., Ltd',
   description: 'クメール関西株式会社は、不要になった家電やプラスチック、アルミ・鉄・不要になった自転車、建設機械、農機具関係を回収・再生し、資源循環に貢献するリサイクル事業を展開しています。',
 };
 
